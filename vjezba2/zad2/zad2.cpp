acbd86e41d192322ee35fce364e8d4247633fbcc
Stuywesant <56384287+Stuywesant@users.noreply.github.com>
2021-10-22 19:20:12 +0200

    Add files via upload

---
+++ vjezba2/zad2/zad2.cpp
@@ -0,0 +1,28 @@
+﻿// zad2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int& funkcija(int* niz, int index, int v) {
+	 if (index > v) 
+	 {
+		 return NULL;
+     }
+
+	return niz[index];
+}
+
+
+
+
+int main()
+{
+	int niz[] = { 1,2,3,4,5 };
+	int index = 2;
+	int v = 5;
+	funkcija(niz, index, v) += 1;
+	for (int i = 0; i < v; i++) {
+		cout << niz[i] << endl;
+	}
+}
---
+++ vjezba2/zad3/zad3.cpp
@@ -0,0 +1,29 @@
+﻿// zad3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+using namespace std;
+#include <iostream>
+
+typedef struct {
+	double x;
+	double y;
+}tocka;
+
+typedef struct {
+	tocka l;
+	tocka r;
+}pravokutnik;
+
+typedef struct {
+	tocka s;
+	double r;
+}kruznica;
+
+
+
+int main()
+{
+	kruznica* niz;
+	niz[0].s.x = 10;
+	std::cout << "Hello World!\n";
+}
