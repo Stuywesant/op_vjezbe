e04ecb1a6401af3609b01dbf17a323e7eb36412d
Stuywesant <56384287+Stuywesant@users.noreply.github.com>
2021-10-28 12:14:15 +0200

    Add files via upload

---
+++ vjezba1/zad3/zad3.cpp
@@ -0,0 +1,50 @@
+﻿// zad3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int FindMin(int arr[], int n) /*Funkcija za minimum*/
+{
+	int i;
+	int min = arr[0];
+	for (i = 0; i < n; i++)
+	{
+		if (min > arr[i])
+			min = arr[i];
+	}
+	return min;
+}
+
+int FindMax(int arr[], int n) /*Funkcija za maximum*/
+{
+	int i;
+	int max = arr[0];
+	for (i = 0; i < n; i++)
+	{
+		if (max < arr[i])
+			max = arr[i];
+	}
+	return max;
+}
+
+int main()
+{
+
+	int n;
+	cout << " Enter the size of the array: ";
+	cin >> n;
+	int i;
+	int* arr = new int[n];
+	cout << "Enter the elements of the array: ";
+	for (i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << "Smallest number in the array is: " << FindMin(arr, n) << endl;
+	cout << "Largest number in the array is: " << FindMax(arr, n) << endl;
+
+	return 0;
+
+}
---
+++ vjezba1/zad4/zad4.cpp
@@ -0,0 +1,59 @@
+﻿// zad4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int FindMinRecursive(int arr[], int n)
+{
+	if (n == 0)
+		return arr[0];
+	else
+	{
+		if (arr[n - 1] < FindMinRecursive(arr, (n - 1)))
+		{
+			return arr[n - 1];
+		}
+		else
+		{
+			return FindMinRecursive(arr, (n - 1));
+		}
+	}
+}
+
+int FindMaxRecursive(int arr[], int n)
+{
+	if (n == 0)
+		return arr[0];
+	else
+	{
+		if (arr[n - 1] > FindMaxRecursive(arr, (n - 1)))
+		{
+			return arr[n - 1];
+		}
+		else
+		{
+			return FindMaxRecursive(arr, (n - 1));
+		}
+	}
+}
+
+
+int main()
+{
+	int n;
+	cout << " Enter the size of the array: ";
+	cin >> n;
+	int i;
+	int* arr = new int[n];
+	cout << "Enter the elements of the array: ";
+	for (i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << "Smallest number in the array is: " << FindMinRecursive(arr, n) << endl;
+	cout << "Largest number in the array is: " << FindMaxRecursive(arr, n) << endl;
+
+	return 0;
+}
